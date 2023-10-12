c5fda48436e9d7d8d4c8154818c00f86892c77c3
Felipe Souza <129899036+felipesr01@users.noreply.github.com>
2023-10-12 19:35:50 -0300

    site final

--- src/paginas/Sabores/index.js
+++ src/paginas/Sabores/index.js
@@ -1,11 +1,52 @@
 import React from "react";
 import Header from "../../componentes/Header";
+import Footer from "../../componentes/Footer";
+import './style.css'
 
 export default function Sabores() {
     return (
-        <div className="sabores">
+        <div>
             <Header/>
-            <h1>Sabores</h1>
+            <div className="sabores">
+                <section className="banner-sabores">
+                    <h1>NOSSOS SABORES</h1>
+                </section>
+                <h2>SABORES DE SORVETE</h2>
+                <section className="area-produtos">
+                    
+                    <div className="card">
+                        <img src="/assets/sabor-oreo.png" alt="Sorvete Oreo"/>
+                        <h3>Sorvete de Oreo</h3>
+                        <p>Delicioso sorvete sabor Oreo. Uma explosão de sabor.</p>
+                    </div>
+                    <div className="card">
+                        <img src="/assets/sabor-pistache.png" alt="Sorvete Pistache"/>
+                        <h3>Sorvete de Pistache</h3>
+                        <p>Cremoso sorvete sabor pistache com pedacinhos de semente.</p>
+                    </div>
+                    <div className="card">
+                        <img src="/assets/sabor-cookies-avela.png" alt="Sorvete Cookies e Avela"/>
+                        <h3>Sorvete de Cookies & Avelã</h3>
+                        <p>O nosso melhor sorvete. Você vai adorar o sabor.</p>
+                    </div>
+                    <div className="card">
+                        <img src="/assets/sorbet-kiwi.png" alt="Sorvete Kiwi"/>
+                        <h3>Sorvete de Kiwi</h3>
+                        <p>Delicioso e refrescante sorvete sabor kiwi. Rico em vitamina C.</p>
+                    </div>
+                    <div className="card">
+                        <img src="/assets/sorbet-morango.png" alt="Sorvete Morango"/>
+                        <h3>Sorvete de Morango</h3>
+                        <p>Sorvete de morango gourmet. Tradicional e saboroso.</p>
+                    </div>
+                    <div className="card">
+                        <img src="/assets/sorbet-limao.png" alt="Sorvete Limao"/>
+                        <h3>Sorvete de Limão Siciliano</h3>
+                        <p>O incrível sorvete gourmet de limão siciliano vai te encantar.</p>
+                    </div>
+                </section>
+            </div>
+            <Footer/>
         </div>
     )
 }
--- src/paginas/Sobre/index.js
+++ src/paginas/Sobre/index.js
@@ -1,11 +1,28 @@
 import React from "react";
 import Header from "../../componentes/Header";
+import './style.css'
+import Footer from "../../componentes/Footer";
 
 export default function Sobre(){
     return (
-        <div className="sobre">
+        <div>
             <Header/>
-            <h1>Sobre</h1>
+            <div className="sobre">
+                <section className="banner-sobre">
+                    <h1>A GELATERIA</h1>
+                </section>
+                <h2>Sobre Nós</h2>
+                <span>Nós simplesmenta amamos sorvete!</span>
+                <div className="txt-sobre">
+                    <p>Somos uma empresa apaixonada pelo que faz. Colocamos amor em cada sorvete produzido. Fazemos o melhor sorvete para os nossos clientes e gostamos de receber elogios. Estamos operando desde 2009 com as melhores matérias-primas para a produção final do sorvete. Vendemos tanto para varejo como para atacado.</p>
+                    <p>Nossos clientes podem comprar os nossos sorvetes e degustar na nossa loja ou levar para sua residência e aproveitar junto com a família. Também vendemos para estabelecimentos e criamos eventos como festa de aniversário, formaturas e eventos empresariais. Para contratar os nossos serviços, basta entrar em contato conosco. Iremos proporcionar o melhor atendimento e os melhores produtos para você fazer a sua festa mais saborosa, com o melhor sorvete da cidade.</p>
+                </div>
+                <div className="fotos-sobre">
+                    <img src="/assets/sobre-image.jpg" alt="Sorvete"/>
+                    <img src="/assets/sorveteria.jpg" alt="Sorvete"/>
+                </div>
+            </div>
+            <Footer/>
         </div>
     )
 }
